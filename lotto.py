19f743fbcea84e7afac061cdc5ef3d1429e166fb
Lárus Þorvaldsson <larust@gmail.com>
2014-05-04 23:12:35 +0000

    same function for vector and scalar input

--- lotto.py
+++ lotto.py
@@ -3,7 +3,7 @@ from scipy.stats import poisson
 from scipy.misc import comb
 import matplotlib.pyplot as plt
 
-def lotteryProfit(lastWin, addWin):
+def lotteryROI(lastWin, addWin):
     #Icelandic lottery param
     tPrice=130
     rPay=0.45
@@ -12,38 +12,9 @@ def lotteryProfit(lastWin, addWin):
     balls=5
     NsplitMax=21
 
-    #Number of possible winners
-    Nsplit=np.arange(0,NsplitMax)
-    
-    #Probability of winning
-    Ncomb=comb(numbers,balls)
-    pWin=1./Ncomb
-
-    #Number of rows bought
-    N=addWin/(rPay*rFP*tPrice)
-
-    #Probability of splitting the jackpot
-    p=poisson.pmf(Nsplit, pWin*N)
-
-    #ROI of buying all possible rows
-    costAll=Ncomb*tPrice
-    myWin=lastWin+addWin+costAll*rPay*rFP
-    smallerWin=(1.-rFP)*rPay*costAll
-    weightedWin=np.sum(p/(Nsplit+1.)*myWin)
-    totWin=weightedWin+smallerWin
-    profit=totWin-costAll
-    rprof=profit/costAll
-    return rprof
-
-
-def lotteryProfitMat(lastWin, addWin):
-    #Icelandic lottery param
-    tPrice=130
-    rPay=0.45
-    rFP=0.57
-    numbers=40
-    balls=5
-    NsplitMax=21
+    #Make sure input is a numpy array
+    lastWin=np.atleast_1d(lastWin)
+    addWin=np.atleast_1d(addWin)
     
     [lastWinMat, addWinMat]=np.meshgrid(lastWin, addWin)
     
